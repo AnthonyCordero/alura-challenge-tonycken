064b2541a579cd1ad3993a5ceb4c4cbfd8a5ecc0
Anthony Cordero <tony170593@gmail.com>
2023-09-03 19:38:04 -0600

    Gender section and carrousel finished - desktop tablet and mobile

--- src/components/PromoVideo/index.jsx
+++ src/components/PromoVideo/index.jsx
@@ -3,7 +3,15 @@ import './styles.css'
 const PromoVideo = () => {
     return <div className='promo-video'>
         <h2>WATCH THIS CONCERT!</h2>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/e65Pirra6AQ?si=hcfJSNItEuSkn5Cl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/e65Pirra6AQ?si=hcfJSNItEuSkn5Cl"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen>
+        </iframe>
     </div>
 
 
